c98344ac2eaa06fc8fe201249ccb95a2f5da1374
Rihabdel <rihababdel@gmail.com>
2026-07-23 17:07:15 +0100

    fix: correction route html

--- FrontendBootstrap/Router/allRoutes.js
+++ FrontendBootstrap/Router/allRoutes.js
@@ -1,26 +1,26 @@
 import Route from "./Route.js";
 
 export const allRoutes = [
-    new Route("/", "Accueil", "/views/home.html", [], "/js/home.js"),
-    new Route("/contact", "Contact", "/views/contact.html", [], "/js/contact.js"),
-    new Route("/recherche", "Recherche", "/views/recherche.html", [], "/js/recherche.js"),
-   
+    new Route("/", "Accueil", "./views/home.html", [], "./js/home.js"),
+    new Route("/contact", "Contact", "./views/contact.html", [], "./js/contact.js"),
+    new Route("/recherche", "Recherche", "./views/recherche.html", [], "./js/recherche.js"),
+
 
     // ADMIN : Vérifie que le fichier est bien à la racine de /pages/
-    new Route("/admin", "Administration", "/views/admin.html", ["ROLE_ADMIN"], "/js/admin.js"),
+    new Route("/admin", "Administration", "./views/admin.html", ["ROLE_ADMIN"], "./js/admin.js"),
 
     // CONNEXION : Dans le sous-dossier auth
-    new Route("/login", "Connexion", "/views/auth/connexion.html", ["disconnected"], "/js/auth/connexion.js"),
+    new Route("/login", "Connexion", "./views/auth/connexion.html", ["disconnected"], "./js/auth/connexion.js"),
 
     // MON COMPTE : Ton fichier s'appelle office.html et est à la racine de /pages/
-    new Route("/monCompte", "Mon Compte", "/views/auth/profil.html", ["ROLE_USER"], "/js/auth/profil.js"),
+    new Route("/monCompte", "Mon Compte", "./views/auth/profil.html", ["ROLE_USER"], "./js/auth/profil.js"),
     // MODIFICATION DU MOT DE PASSE : Ton fichier s'appelle editPassword.html et est à la racine de /pages/
-    new Route("/editPassword", "Modification du mot de passe", "/views/auth/editPassword.html", ["connected"], "/js/auth/editPassword.js"),
+    new Route("/editPassword", "Modification du mot de passe", "./views/auth/editPassword.html", ["connected"], "./js/auth/editPassword.js"),
     // INSCRIPTION : Ton fichier s'appelle inscript.html à la racine de /pages/
-    new Route("/inscription", "Inscription", "/views/auth/inscription.html", ["disconnected"], "/js/auth/inscription.js"),
+    new Route("/inscription", "Inscription", "./views/auth/inscription.html", ["disconnected"], "./js/auth/inscription.js"),
 
-    new Route("/404", "Page introuvable", "/views/404.html", []),
-    new Route("/trajets", "Mes trajets", "/views/trajets.html", ["ROLE_USER", "ROLE_ADMIN", "ROLE_CONDUCTEUR"], "/js/trajets.js"),
+    new Route("/404", "Page introuvable", "./views/404.html", []),
+    new Route("/trajets", "Mes trajets", "./views/trajets.html", ["ROLE_USER", "ROLE_ADMIN", "ROLE_CONDUCTEUR"], "./js/trajets.js"),
 
 ];
 
